bed358a441fcaf1d2acb5a25d428678606d7af50
Krushil20 <krushilb123@gmail.com>
2023-12-01 10:00:25 -0500

    task 1 commit

---
+++ src/task1/random.java
@@ -0,0 +1,16 @@
+package task1;
+
+import java.util.Random;
+
+public class random {
+    public static void main(String[] args) {
+        // create instance of Random class
+        Random rand = new Random();
+
+        // Generate random integers in range 0 to 999
+        int rand_int1 = rand.nextInt(100);
+
+        // Print random integers
+        System.out.println("Random Integers: "+rand_int1);
+    }
+}
